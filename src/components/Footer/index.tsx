d3ced7a49c6625e6c2caa5d2741bcdf06fc04b92
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2022-12-28 21:45:02 -0300

    Add useState and events

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState, ChangeEvent, FormEvent } from "react";
 import "./style.sass";
 import logo from "/images/logob.svg";
 import phone from "/images/icon-phone.svg";
-import email from "/images/icon-email.svg";
+import emailIcon from "/images/icon-email.svg";
 import { IconContext } from "react-icons";
 import { MdFacebook } from "react-icons/md";
 import { AiFillTwitterCircle, AiFillInstagram } from "react-icons/ai";
 
 export default function Footer() {
+  let emailValidation = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  const [email, setEmail] = useState("");
+  const [state, setState] = useState("");
+  function handleEmail(event: ChangeEvent<HTMLInputElement>) {
+    setEmail(event.target.value);
+  }
+
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    if (emailValidation.test(email)) {
+      setState("success");
+    } else {
+      setState("error");
+    }
+  }
+
   return (
     <IconContext.Provider value={{ color: "#fff", size: "3em" }}>
       <footer>
@@ -22,7 +39,7 @@ export default function Footer() {
             <p>Phone: +1-543-123-4567</p>
           </article>
           <article className="email-container">
-            <img src={email} alt="email logo" />
+            <img src={emailIcon} alt="email logo" />
             <p>example@huddle.com</p>
           </article>
           <ul className="social-links">
@@ -43,9 +60,26 @@ export default function Footer() {
             To recieve tips on how to grow your community, sign up to our weekly
             newsletter. We’ll never send you spam or pass on your email address
           </p>
-          <form>
-            <input placeholder="Email Address" type="text" />
+          <form onSubmit={handleSubmit}>
+            <input
+              placeholder="Email Address"
+              value={email}
+              type="text"
+              onChange={handleEmail}
+            />
             <button>Subscribe</button>
+            <div className="state-container">
+              {state === "success" && (
+                <p className="success-state">
+                  Email sign up has been successful
+                </p>
+              )}
+              {state === "error" && (
+                <p className="error-state">
+                  Please provide a valid email address!
+                </p>
+              )}
+            </div>
           </form>
         </section>
       </footer>
